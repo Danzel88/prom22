1c821f983661e79ab798d356cfabc43495892f13
Danzel88 <gecnepago@gmail.com>
2023-05-25 23:57:21 +0300

    Fix счетчика в отправке сообщений в канал

--- bot.py
+++ bot.py
@@ -51,9 +51,9 @@ async def main():
     logger.info("Starting bot")
     storage = RedisStorage2('localhost',
                             6379,
-                            db=4,
+                            db=8,
                             pool_size=10,
-                            prefix='Graduates22') if config.tg_bot.use_redis else MemoryStorage()
+                            prefix='Graduates23') if config.tg_bot.use_redis else MemoryStorage()
     bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
     dp = Dispatcher(bot, storage=storage)
 
